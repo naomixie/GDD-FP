111b93ba2512ee8841e7fe4750b88af8f92cb123
Naomi <41462684+naomixie@users.noreply.github.com>
2021-06-23 14:33:42 +0800

    fix: 调参

--- GameDD_FP/Assets/ToolMovement.cs
+++ GameDD_FP/Assets/ToolMovement.cs
@@ -8,7 +8,7 @@ public class ToolMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        transform.position = new Vector3(Input.mousePosition.x / 1920 * 20 - 10, Input.mousePosition.y / 1080 * 11f - 6f, InitialDepth);
     }
 
     // Update is called once per frame
@@ -19,6 +19,30 @@ public class ToolMovement : MonoBehaviour
 
     void UpdatePosition()
     {
-        transform.position = new Vector3(Input.mousePosition.x / 1920 * 20 - 10, Input.mousePosition.y / 1080 * 11f - 6f, InitialDepth);
+        Debug.Log(Input.GetAxis("Mouse ScrollWheel"));
+        if (Input.GetAxis("Mouse ScrollWheel") > 0) // forward
+        {
+            transform.position = new Vector3(Input.mousePosition.x / 1920 * 20 - 10, Input.mousePosition.y / 1080 * 11f - 6f, transform.position.z + Input.GetAxis("Mouse ScrollWheel"));
+
+        }
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0) // back
+        {
+            transform.position = new Vector3(Input.mousePosition.x / 1920 * 20 - 10, Input.mousePosition.y / 1080 * 11f - 6f, transform.position.z + Input.GetAxis("Mouse ScrollWheel"));
+
+        }
+        else if (Input.GetKey(KeyCode.Z))
+        {
+            transform.position = new Vector3(Input.mousePosition.x / 1920 * 20 - 10, Input.mousePosition.y / 1080 * 11f - 6f, transform.position.z + 0.1f);
+        }
+        else if (Input.GetKey(KeyCode.X)) // back
+        {
+            transform.position = new Vector3(Input.mousePosition.x / 1920 * 20 - 10, Input.mousePosition.y / 1080 * 11f - 6f, transform.position.z - 0.1f);
+
+        }
+        else
+        {
+            transform.position = new Vector3(Input.mousePosition.x / 1920 * 20 - 10, Input.mousePosition.y / 1080 * 11f - 6f, transform.position.z);
+        }
+
     }
 }
